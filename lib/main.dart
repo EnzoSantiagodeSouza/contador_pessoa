5f568548a5e658a863efa268da25a2087d93588a
Enzo Santiago <enzosantiagodesouza@gmail.com>
2026-09-18 10:40:31 -0300

    2 - Adicionando elementos

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        color: Colors.black,
-        alignment: Alignment.center,
-        child: Text("Heloo World!"))
+      home: HomePage()
     );
   }
 }
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       home: Scaffold(body: Center(child: Text('Hello World!'))),
-//     );
-//   }
-// }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Pode Entrar!',
+          style: TextStyle(
+              fontSize: 26,
+              color: Color.fromARGB(255, 4, 87, 154),
+              fontWeight: FontWeight.w700
+            ),
+          ),
+          Text('0',
+          style: TextStyle(
+              fontSize: 26,
+              color: Color.fromARGB(255, 4, 87, 154),
+              fontWeight: FontWeight.w700
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
